5257e5790761158c743ca8a9e01b61e9eb61a5c0
Maksim <mak.lis2016@gmail.com>
2022-11-08 23:06:06 +0300

    add swr and fix sizes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,20 @@ import { collections } from '../data/collections'
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import ScrollContainer from 'react-indiana-drag-scroll'
+import useSWR from 'swr'
+import React from 'react'
+import { ICollection } from '../interfaces/ICollection'
 
 export default function Home() {
+	// @ts-ignore
+	// const fetcher = (...args: any) => fetch(...args).then(res => res.json())
+
+	// const IMAGE_API = 'http://localhost:3000/'
+	// const { data: collections, error } = useSWR(
+	// 'http://localhost:3000/collections',
+	// fetcher
+	// )
+
 	return (
 		<>
 			<Head>
@@ -17,11 +29,20 @@ export default function Home() {
 						vertical={false}
 						className={styles.collections}
 						nativeMobileScroll={false}>
-						{collections.map(card => (
-							<div style={{ marginRight: '2rem', paddingTop: '1rem' }}>
-								<CollectionCard {...card} key={card.title} />
-							</div>
-						))}
+						{collections &&
+							collections
+								// .map(card => ({
+								// 	...card,
+								// 	backgroundImage: IMAGE_API + card.backgroundImage,
+								// 	image: IMAGE_API + card.image,
+								// }))
+								.map(card => (
+									<div
+										style={{ marginRight: '2rem', paddingTop: '1rem' }}
+										key={card.title}>
+										<CollectionCard {...card} />
+									</div>
+								))}
 					</ScrollContainer>
 				</div>
 			</div>
